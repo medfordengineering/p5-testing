11da633cf3d79d456e706f4dcbf6f1d8a6b0d83f
medfordengineering <schristy@medford.k12.ma.us>
2021-02-26 13:18:11 -0500

    hhh
    hhhh

--- mySketch.js
+++ mySketch.js
@@ -1,35 +1,39 @@
-let angle = 0
+//Set up an array to hold my objects
+let mazus = []
 
 function setup() {
 	createCanvas(800, 800);
-	background('white');
-	noStroke()
-	angleMode(DEGREES)
-	rectMode(CENTER);
+	background('lightblue');
+
 }
 
 function draw() {
-	angle += 30;
-	ratio = width / 8;
-	for (i = ratio; i < width; i += ratio) {
-		spin(width/2, i, 'cornflowerblue', 50, angle);
+	background('lightblue')
+	//Place the mazus on the canvas
+	for (i = 0; i < mazus.length; i++) {
+		mazus[i].show()
 	}
 }
 
-function spin(tx, ty, squareColor, s, r) {
-	fill(squareColor)
-	push();
-	translate(tx, ty)
-	rotate(r)
-	rect(0, 0, s)
-	circleColor = color('pink')
-	circleColor.setAlpha(40)
-	fill(circleColor)
-	ellipse(-s / 2, -s / 2, s / 3);
-	ellipse(s / 2, -s / 2, s / 3);
-	ellipse(-s / 2, s / 2, s / 3);
-	ellipse(s / 2, s / 2, s / 3);
-	fill('cyan')
-	ellipse(0, 0, s / 1.5, s / 2);
-	pop();
+function mouseDragged() {
+	//Create a new mazu for every mouse press
+	let mazu = new Mazu(mouseX, mouseY)
+	mazus.push(mazu);
+}
+
+class Mazu {
+	constructor(x, y) {
+		this.x = x;
+		this.y = y;
+		this.color = 'darkorange'
+		this.size = 10
+	}
+	show() {
+		noStroke()
+		fill(this.color)
+		triangle(this.x, this.y, this.x + this.size, this.y + this.size, this.x - this.size, this.y + this.size)
+		fill('darkorchid')
+		ellipse(this.x, this.y, this.size / 2);
+	}
+	
 }
