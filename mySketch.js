e7822cbceba9e50edb7b57199b10843c1061323b
medfordengineering <schristy@medford.k12.ma.us>
2021-02-26 11:04:34 -0500

    new page
    changed the page

--- mySketch.js
+++ mySketch.js
@@ -1,27 +1,35 @@
-let x = 400;
-let y = 500;
-let r = 200
-let rn = 400
-
-let mode = true;
-
+let angle = 0
 
 function setup() {
-	createCanvas(windowWidth, windowHeight);
-	background(100);
+	createCanvas(800, 800);
+	background('white');
+	noStroke()
+	angleMode(DEGREES)
 	rectMode(CENTER);
 }
 
 function draw() {
-	
-	let s = rn/r
-	fill('cyan')
-	ellipse(x, y, r);
-	fill('yellow')
-	rect(x, y + 50/s, 50/s);
+	angle += 30;
+	ratio = width / 8;
+	for (i = ratio; i < width; i += ratio) {
+		spin(width/2, i, 'cornflowerblue', 50, angle);
+	}
 }
 
-function mouseClicked() {
-	mode = !mode;
-	print(mode)
+function spin(tx, ty, squareColor, s, r) {
+	fill(squareColor)
+	push();
+	translate(tx, ty)
+	rotate(r)
+	rect(0, 0, s)
+	circleColor = color('pink')
+	circleColor.setAlpha(40)
+	fill(circleColor)
+	ellipse(-s / 2, -s / 2, s / 3);
+	ellipse(s / 2, -s / 2, s / 3);
+	ellipse(-s / 2, s / 2, s / 3);
+	ellipse(s / 2, s / 2, s / 3);
+	fill('cyan')
+	ellipse(0, 0, s / 1.5, s / 2);
+	pop();
 }
